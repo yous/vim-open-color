cf449cf59ff7e588fabd6154c93fb9311e988106
Chayoung You <yousbe@gmail.com>
2021-07-26 03:18:11 +0900

    Fix ci/generate.sh

--- ci/generate.sh
+++ ci/generate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-gvim -V1 -f -T xterm-256color -E -n -u ci/init.vim -S ci/"$1".vim ci/vimcolors.rb
+"$1" -V1 -f -T xterm-256color -E -n -u ci/init.vim -S ci/"$2".vim ci/vimcolors.rb
 sleep 3
-[ -e dist/"$1".html ]
+[ -e dist/"$2".html ]
