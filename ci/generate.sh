0d95c1afbca2757308faa9821715d45259704b6b
Chayoung You <yousbe@gmail.com>
2021-07-25 22:45:33 +0900

    Specify -f flag to gvim
    - https://github.com/vim/vim/issues/4213

--- ci/generate.sh
+++ ci/generate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-gvim -T xterm-256color -E -n -u ci/init.vim -S ci/"$1".vim ci/vimcolors.rb >/dev/null
+gvim -V1 -f -T xterm-256color -E -n -u ci/init.vim -S ci/"$1".vim ci/vimcolors.rb
 sleep 3
 [ -e dist/"$1".html ]
